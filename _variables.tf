568d4f608ea00b29a4e1c28eff3f1a5512a64c93
karandeep-singh <root@ip-10-90-59-149.us-west-2.compute.internal>
2023-02-08 14:26:45 +0000

    added descriptions

--- _variables.tf
+++ _variables.tf
@@ -1,15 +1,20 @@
 variable "instanceid" {
   description = "EC2 instance id for schedule"
+  type        = string
 }
 
 variable "cron_stop" {
   description = "Cron expression to define when to trigger a stop of the EC2"
+  type        = string
 }
 
 variable "cron_start" {
   description = "Cron expression to define when to trigger a start of the EC2"
+  type        = string
 }
 
 variable "enable" {
-  default = true
+  description = "Controls if resources should be created (it affects almost all resources)"
+  default     = true
+  type        = bool
 }
